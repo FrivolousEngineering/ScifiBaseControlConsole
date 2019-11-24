842c4a146f367175078182d95c0b2d77e94c48d6
Jaime van Kessel <nallath@gmail.com>
2019-11-24 18:37:10 +0100

    Clean up some code

--- AnimatedLine.py
+++ AnimatedLine.py
@@ -7,20 +7,20 @@ class AnimatedLine(QObject):
     def __init__(self):
         super(AnimatedLine, self).__init__()
         self._color = QColor(255, 0, 0)
-        #self.setLine(0, 0, 200, 200)
-        #self.setPen(QPen(self._color, 5))
 
         self._space = 4
+
         self._dash_shift = 0
         self._dashes = [max(0, self._dash_shift - self._space), min(self._dash_shift, 4), self._space, self._space, min(2 * self._space - self._dash_shift, 4), max(self._space - self._dash_shift, 0)]
 
         self.line = QGraphicsPathItem()
 
         path = QPainterPath()
-        ctr_point1 = QPointF(50, 200)
-        ctr_point2 = QPointF(150, 200)
+        ctr_point1 = QPointF(75, 0)
+        ctr_point2 = QPointF(125, 200)
         path.cubicTo(ctr_point1, ctr_point2, QPointF(200, 200))
         self.line.setPath(path)
+
         self.anim = QPropertyAnimation(self, b'dashShift')
         self.anim.setDuration(1000)
         self.anim.setStartValue(0.)
@@ -29,21 +29,23 @@ class AnimatedLine(QObject):
 
         self.anim.start()
 
-    def setColor(self, color):
-        print("UPDATING COLOR YO!", color)
-        self._color = color
+    def _recreatePen(self):
         pen = QPen()
-        self.line.setPen(QPen(self._color, 5))
+        pen.setColor(self._color)
+        pen.setDashPattern(self._dashes)
+        pen.setWidth(5)
+        return pen
+
+    def setColor(self, color):
+        self._color = color
+        self.line.setPen(self._recreatePen())
 
     def setDashShift(self, dash_shift):
         self._dash_shift = dash_shift
 
         self._dashes = [max(0, self._dash_shift - self._space), min(self._dash_shift, 4), self._space, self._space, min(2 * self._space - self._dash_shift, 4), max(self._space - self._dash_shift, 0)]
-        pen = QPen()
-        pen.setColor(self._color)
-        pen.setDashPattern(self._dashes)
-        pen.setWidth(5)
-        self.line.setPen(pen)
+
+        self.line.setPen(self._recreatePen())
 
     dashShift = Property(float, fset= setDashShift)
     color = Property(QColor, fset= setColor)
