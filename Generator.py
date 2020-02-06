e5cbd9e9de77b47ad84de8882894ffff4c9d7c9d
Jaime van Kessel <nallath@gmail.com>
2020-02-06 18:19:06 +0100

    Remove old unused code

--- Generator.py
+++
@@ -1,8 +0,0 @@
-from Node import Node
-
-
-class Generator(Node):
-    def __init__(self, name, parent=None):
-        super(Generator, self).__init__(name, parent)
-        self.setSupportedInputs(["water", "fuel"])
-        self.setSupportedOutputs(["energy"])
