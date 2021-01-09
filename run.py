4b89cb43b13542625780d16ff91f153832a4d5c9
Jaime van Kessel <nallath@gmail.com>
2021-01-09 14:05:47 +0100

    Get modifier data from server

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkReply, QNetworkRequest
 from PyQt5.QtQml import qmlRegisterType
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQuick import QQuickView
@@ -7,6 +8,7 @@ from PyQt5.QtCore import pyqtProperty as Property
 from PyQt5.QtCore import pyqtSlot as Slot
 
 from PyQt5.QtQml import QQmlApplicationEngine,QQmlEngine, QQmlComponent
+import json
 
 from Node import Node
 from RadialBar import RadialBar
@@ -15,14 +17,38 @@ import sys
 
 class TestObject(QObject):
     serverReachableChanged = Signal()
+    modifiersChanged = Signal()
 
     def __init__(self, parent=None):
         QObject.__init__(self, parent)
         self._data = [ Node("generator_1"),  Node("fluid_cooler_2"), Node("battery_2"), Node("generator_2"), Node("fluid_cooler_1"),  Node("battery_1"), Node("water_storage_1"), Node("water_storage_2"), Node("rain_collector_2"), Node("hydroponics_2"), Node("oxygen_storage"), Node("water_purifier")]
         self._server_reachable = False
 
+        self._network_manager = QNetworkAccessManager()
+        self._network_manager.finished.connect(self._onNetworkFinished)
+
         for node in self._data:
             node.serverReachableChanged.connect(self.serverReachableChanged)
+
+        # This is pretty static data so we only need to request this once.
+        modifier_data_url = "http://localhost:5000/modifier/"
+        self._network_manager.get(QNetworkRequest(QUrl(modifier_data_url)))
+
+        self._modifiers = []
+
+    def _onNetworkFinished(self, reply: QNetworkReply):
+        status_code = reply.attribute(QNetworkRequest.HttpStatusCodeAttribute)
+        if status_code == 404:
+            print("server was not found!")
+            # TODO: handle this case, since it won't retry. 
+            return
+        data = bytes(reply.readAll())
+        self._modifiers = json.loads(data)
+        self.modifiersChanged.emit()
+
+    @Property("QVariantList", notify = modifiersChanged)
+    def modifierData(self):
+        return self._modifiers
 
     @Property("QVariantList", constant=True)
     def nodeData(self):
